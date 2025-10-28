521fc81cec5a43cc5dd58d79841758f55310e2c8
Yuri <souzasantos@yu.com>
2025-10-27 23:44:52 -0300

    new folder structure, new classes

---
+++ day06/01_functions.py
@@ -0,0 +1,17 @@
+# %%
+
+print("Hello, world")
+
+# %%
+
+value = input("Enter a value: ")
+print(value)
+
+# %%
+
+def f(x):
+    result = 1 + x
+    return result
+# %%
+
+f(10)
---
+++ day06/02_compound_interests.py
@@ -0,0 +1,24 @@
+# %%
+
+def compound_interests(amount, interest, years):
+    '''Calculate the financial return due to a given amount through years and a given interest rate.
+    
+    amount:
+        integer number that represents the value you want to enter
+
+    interest:
+        a float number between 0 and 1 that represets the interest rate
+
+    years:
+        a integer number >= 1 that represents the time of investment
+
+    '''
+    return amount * (1 + interest) ** years
+
+# %%
+
+compound_interests(interest=0.13, years=4, amount=1000)
+
+# %%
+
+print()
